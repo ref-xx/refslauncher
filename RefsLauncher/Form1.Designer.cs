b554851d82983847e75c60552dc6abe66d8a1689
Ref <ref-xx@users.noreply.github.com>
2025-03-11 19:12:28 +0300

    VC# 2010 .net4 client profile

---
+++ RefsLauncher/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.chkFullAmiga = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(257, 264);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(175, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Launch &Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Search:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(78, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "&Options";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.chkFullAmiga);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 79);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(256, 257);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            this.groupBox1.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(18, 19);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(120, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Exit after Launching";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(93, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "WHD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Games Folder";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(159, 228);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Save Config";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(160, 108);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Rescan";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.Location = new System.Drawing.Point(93, 143);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(145, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "SYS";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "WB Folder";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 46);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Tip: Exit games with ALT+F4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Resolution";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.Location = new System.Drawing.Point(93, 178);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(145, 20);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Text = "800x600";
+            // 
+            // chkFullAmiga
+            // 
+            this.chkFullAmiga.AutoSize = true;
+            this.chkFullAmiga.Checked = true;
+            this.chkFullAmiga.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkFullAmiga.Location = new System.Drawing.Point(159, 204);
+            this.chkFullAmiga.Name = "chkFullAmiga";
+            this.chkFullAmiga.Size = new System.Drawing.Size(79, 17);
+            this.chkFullAmiga.TabIndex = 14;
+            this.chkFullAmiga.Text = "Full Screen";
+            this.chkFullAmiga.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = " F12 to Emulator menu";
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(6, 228);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(91, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "About...";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 350);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(217, 330);
+            this.Name = "Form1";
+            this.Text = "RefLauncher v0.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox chkFullAmiga;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ RefsLauncher/Form1.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Focus();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+
+        {
+            loadConfig();
+
+            LoadDirectories();
+            textBox1.Select();
+
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void LoadDirectories()
+        {
+            string whdPath = Path.Combine(Application.StartupPath, textBox2.Text);
+            if (Directory.Exists(whdPath))
+            {
+                string[] directories = Directory.GetDirectories(whdPath)
+                                               .Select(Path.GetFileName)
+                                               .OrderBy(name => name, StringComparer.OrdinalIgnoreCase)
+                                               .ToArray();
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(directories);
+            }
+            else
+            {
+                MessageBox.Show("WHD klasörü bulunamadı!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = textBox1.Text.ToLower();
+            if (string.IsNullOrWhiteSpace(searchText)) return;
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                string itemText = listBox1.Items[i].ToString().ToLower();
+                if (itemText.Contains(searchText))
+                {
+                    listBox1.SelectedIndex = i;
+                    listBox1.TopIndex = i;
+                    return;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = !groupBox1.Visible;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveConfig();
+            groupBox1.Visible = false;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            LoadDirectories();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            run();
+        }
+
+        private void run()
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedFolder = listBox1.SelectedItem.ToString();
+                bool exitLauncher = checkBox1.Checked;
+                replaceResolution();
+                launch(selectedFolder, exitLauncher);
+            }
+            else
+            {
+                MessageBox.Show("Lütfen bir öğe seçin!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+            private void launch(string folderName, bool exitLauncher)
+        {
+            string sysPath = Path.Combine(Application.StartupPath, "SYS", "S");
+            string configFilePath = Path.Combine(sysPath, "User-Startup");
+            
+            try
+            {
+                // Yeni içerik oluştur
+                                string newContent = "cd DH1:" + folderName + ";\n" +
+                                    "WHDLoad " + folderName + ".slave PRELOAD;\n";
+                
+                // Dosyanın içeriğini güncelle
+                File.WriteAllText(configFilePath, newContent);
+                
+                // WinUAE'yi başlat
+                string wuaePath = Path.Combine(Application.StartupPath, "WUAE", "WinUae.exe");
+                Process.Start(wuaePath, "-f RefLauncher.uae -portable");
+                
+                // exitLauncher true ise uygulamadan çık
+                if (exitLauncher)
+                {
+                    Application.Exit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Başlatma hatası"+ ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+            private void replaceResolution()
+            {
+                string configPath = Path.Combine(Application.StartupPath, "WUAE", "RefLauncher.uae");
+
+                if (!File.Exists(configPath))
+                {
+                    MessageBox.Show("Config dosyası bulunamadı!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                string resolutionText = textBox4.Text.Trim();
+                string[] parts = resolutionText.Split('x');
+
+                if (parts.Length != 2)
+                {
+                    MessageBox.Show("Geçersiz çözünürlük formatı! Lütfen '800x600' şeklinde girin.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                int width, height;
+
+                try
+                {
+                    width = int.Parse(parts[0].Trim());
+                    height = int.Parse(parts[1].Trim());
+                }
+                catch
+                {
+                    MessageBox.Show("Geçersiz çözünürlük değerleri! Lütfen sayısal değerler girin.", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                string fullAmigaValue = chkFullAmiga.Checked ? "true" : "false";
+
+                try
+                {
+                    string[] lines = File.ReadAllLines(configPath);
+
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        if (lines[i].StartsWith("gfx_width_fullscreen="))
+                        {
+                            lines[i] = "gfx_width_fullscreen=" + width;
+                        }
+                        else if (lines[i].StartsWith("gfx_height_fullscreen="))
+                        {
+                            lines[i] = "gfx_height_fullscreen=" + height;
+                        }
+                        else if (lines[i].StartsWith("gfx_fullscreen_amiga="))
+                        {
+                            lines[i] = "gfx_fullscreen_amiga=" + fullAmigaValue;
+                        }
+                    }
+
+                    File.WriteAllLines(configPath, lines);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Çözünürlük güncellenirken hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+
+
+
+            private void saveConfig()
+            {
+                string configPath = Path.Combine(Application.StartupPath, "WUAE", "launcher.ini");
+
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(configPath))
+                    {
+                        writer.WriteLine("Resolution=" + textBox4.Text.Trim());
+                        writer.WriteLine("Path=" + textBox2.Text.Trim());
+                        writer.WriteLine("SomeSetting=" + textBox3.Text.Trim());
+                        writer.WriteLine("FullAmiga=" + (chkFullAmiga.Checked ? "true" : "false"));
+                        writer.WriteLine("ExitLauncher=" + (checkBox1.Checked ? "true" : "false"));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ayarlar kaydedilirken hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            private void loadConfig()
+            {
+                string configPath = Path.Combine(Application.StartupPath, "WUAE", "launcher.ini");
+
+                if (!File.Exists(configPath))
+                    return; // Dosya yoksa işlem yapma
+
+                try
+                {
+                    string[] lines = File.ReadAllLines(configPath);
+
+                    foreach (string line in lines)
+                    {
+                        string[] parts = line.Split('=');
+                        if (parts.Length != 2) continue;
+
+                        string key = parts[0].Trim();
+                        string value = parts[1].Trim();
+
+                        switch (key)
+                        {
+                            case "Resolution":
+                                textBox4.Text = value;
+                                break;
+                            case "Path":
+                                textBox2.Text = value;
+                                break;
+                            case "SomeSetting":
+                                textBox3.Text = value;
+                                break;
+                            case "FullAmiga":
+                                chkFullAmiga.Checked = value.ToLower() == "true";
+                                break;
+                            case "ExitLauncher":
+                                checkBox1.Checked = value.ToLower() == "true";
+                                break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ayarlar yüklenirken hata oluştu: " + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            private void button5_Click(object sender, EventArgs e)
+            {
+                MessageBox.Show("RefLauncher by Ref 2025 (c) retrojen.org  (Based On Ozay Turay's exe packer)");
+            }
+
+            private void textBox1_KeyDown(object sender, KeyEventArgs e)
+            {
+                if (e.KeyCode == Keys.Enter) // Enter tuşuna basıldığını kontrol et
+                {
+                    e.SuppressKeyPress = true; // Enter tuşunun "ding" sesini engelle
+                    button1.Focus(); // Button1'e odaklan
+                }
+            }
+
+
+        
+    }
+}
